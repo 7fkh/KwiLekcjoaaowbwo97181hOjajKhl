0a600d5347bb36be41dc2c4b3f502b60891c1163
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-20 18:40:17 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,44 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
 
-export default{
-  components:{
+// استيراد Swiper و Autoplay
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+export default {
+  components: {
     NavBar,
-    Footer
+    Footer,
+    Swiper,
+    SwiperSlide
   },
   data() {
     return {
-      images:[
+      feedbackList: [
+        {
+          id: 1,
+          username: "kg.r",
+          avatar: "",
+          text: "أفضل متجر حرفياً وأضمن سيرفر"
+        },
+        {
+          id: 2,
+          username: "naseer1319",
+          avatar: "",
+          text: "أنصحكم فيه، الأفضل بلا منازع"
+        },
+        {
+          id: 3,
+          username: "3we6",
+          avatar: "",
+          text: "أي شيء تبغاه إن شاء الله موجود هناك وضمان مرة أسطوري وتعامل حلو"
+        }
       ]
     };
-  },
+  }
 }
 </script>
 
@@ -23,288 +48,82 @@ export default{
       <NavBar />
       <div class="text">
         <h1>خلي ستور</h1>
-        <h2>متجر رقمي متخصص في تقديم خدمات البرمجة وتطوير الحلول التقنية والمواقع الالكترونيه</h2>
+        <h2>متجر رقمي متخصص في تقديم خدمات البرمجة وتطوير الحلول التقنية والمواقع الإلكترونية</h2>
         <div class="btns">
           <RouterLink class="btn" to="/products">المنتجات</RouterLink>
-          <RouterLink class="btn" to="/order">اطلب الان</RouterLink>
+          <RouterLink class="btn" to="/order">اطلب الآن</RouterLink>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,128L48,149.3C96,171,192,213,288,224C384,235,480,213,576,186.7C672,160,768,128,864,133.3C960,139,1056,181,1152,186.7C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#ffffff" fill-opacity="1"
+              d="M0,128L48,149.3C96,171,192,213,288,224C384,235,480,213,576,186.7C672,160,768,128,864,133.3C960,139,1056,181,1152,186.7C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z" />
+      </svg>
     </div>
-    <div class="feedbacks">
-      <h1>الأراء <font-awesome-icon :icon="['fas', 'comment']" /></h1>
-      <div class="cont">
-        <div data-aos="fade-right" data-aos-duration="1500" class="cont1">
-                    <div class="top">
-                        <div class="left">
-                          <img src="">
-                          <p>kg.r</p>
-                        </div>
-                        <div class="right">
-                            <p>✨✨✨✨✨</p>
-                        </div>
-                    </div>
-                    <h3>افضل متجر حرفيا واضمن سيرفر</h3>
-                    </div>
-                    <div data-aos="fade-right" data-aos-duration="1500" class="cont1">
-                    <div class="top">
-                        <div class="left">
-                          <img src="">
-                          <p>naseer1319</p>
-                        </div>
-                        <div class="right">
-                            <p>✨✨✨✨✨</p>
-                        </div>
-                    </div>
-                    <h3>انصحكم فيه الافضل بلا منازع</h3>
-                    </div>
-                    <div data-aos="fade-right" data-aos-duration="1500" class="cont1">
-                    <div class="top">
-                        <div class="left">
-                            <img src="">
-                        <p>3we6</p>
-                        </div>
-                        <div class="right">
-                            <p>✨✨✨✨✨</p>
-                        </div>
-                    </div>
-                    <h3>اي شي تبغاه ان شاء الله موجود هناك وضمان مرة اسطوري وتعامل حلو</h3>
-                    </div>
-      </div>
+
+    <div class="feedbacks-slider">
+      <h1>شهادة عملائنا لنا - KhLi StoRe</h1>
+      <Swiper
+        :modules="[Autoplay]"
+        :slides-per-view="1"
+        :loop="true"
+        :autoplay="{ delay: 4000, disableOnInteraction: false }"
+        class="testimonial-swiper"
+      >
+        <SwiperSlide v-for="feedback in feedbackList" :key="feedback.id">
+          <div class="testimonial-card">
+            <div class="top">
+              <img :src="feedback.avatar || '/IMG_1500.webp'" />
+              <p>{{ feedback.username }}</p>
+            </div>
+            <h3>{{ feedback.text }}</h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
+
     <Footer />
   </main>
 </template>
 
-
 <style scoped>
-.cont img{
-  border-radius:8px;
-  width:40%;
+/* أضف تنسيق CSS المناسب هنا حسب تنسيقاتك السابقة */
+.feedbacks-slider {
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-
-.card{
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  padding:20px 20px;
-  border-radius:10px;
-  gap:30px;
+.feedbacks-slider h1 {
+  font-size: 50px;
+  color: #3e404e;
+  margin-bottom: 30px;
 }
-
-  .home svg{
-    margin-top:auto;
-  }
-.card h2{
-  font-size:30px;
-  color:#3e404e;
+.testimonial-swiper {
+  width: 90%;
+  max-width: 500px;
 }
-
-.card .info{
-  width:100%;
-  font-size:20px;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:end;
+.testimonial-card {
+  background-color: whitesmoke;
+  padding: 20px;
+  border-radius: 10px;
+  border-top: 4px solid black;
+  border-bottom: 4px solid black;
+  text-align: right;
 }
-
-.cont{
-  margin-top:100px;
-  width:100%;
-  display:flex;
-  flex-direction:row;
-  gap:80px;
-  justify-content:center;
-  flex-wrap:wrap;
+.testimonial-card .top {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
-
-.card .price{
-  width:100%;
-  direction:rtl;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:start;
-  gap:10px;
+.testimonial-card img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #3e404e;
 }
-
-.prices h1,.feedbacks h1,.works h1{
-  color:#3e404e;
-  font-size:50px;
-}
-
-main{
-  width:100%;
-  height:100%;
-  overflow:hidden;
-}
-
-.prices{
-  margin-top:100px;
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-}
-
-.feedbacks{
-  margin-top:100px;
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-}
-
-.works{
-  margin-top:100px;
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-}
-
-.home{
-  width:100%;
-  height:100vh;
-  background-color:#000000;
-  background-size:cover;
-}
-
-.text{
-  text-align:center;
-  margin-top:150px;
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  gap:8px;
-}
-
-.text h1{
-  color:white;
-  font-size:55px;
-  text-shadow:1px 1px 3px rgba(0, 0, 0, 0.425);
-}
-
-.text h2{
-  color:rgb(255, 255, 255);
-  font-weight:lighter;
-  width:40%;
-  text-shadow:2px 2px 5px rgba(0, 0, 0, 0.377);
-}
-
-.btns{
-  margin-top:10px;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  gap:20px;
-}
-
-.btn{
-  text-decoration:none;
-  color:#333;
-  padding:8px 30px;
-  border-radius:25px;
-  background-color:white;
-  cursor:pointer;
-  box-shadow:2px 2px 5px rgba(0, 0, 0, 0.377);
-}
-
-.feedbacks img{
-  border-radius:100%;
-  background-color:#3e404e;
-  width:50px;
-  height:50px;
-  display:none;
-}
-
-.cont1{
-    border-radius:8px;
-    background-color:whitesmoke;
-    width:400px;
-    padding:5px 10px;
-    height:250px;
-    display:flex;
-    flex-direction:column;
-    border-top:4px solid #000000;
-    border-bottom:4px solid #000000;
-}
-
-.top{
-    margin-top:15px;
-    display:flex;
-    justify-content:space-around;
-    align-items:center;
-}
-
-.left{
-    display:flex;
-    justify-content:space-around;
-    align-items:center;
-}
-
-.top img{
-    width:40px;
-    margin-right:10px;
-}
-
-.cont1 h3{
-    text-align:end;
-    margin-top:60px;
-    padding:5px 5px;
-}
-
-@media screen and (max-width: 768px) {
-  .home svg{
-    display:none;
-  }
-
-  .home{
-    min-height:100vh;
-  }
-
-  .home .text h2{
-    width:90%;
-  }
-
-  .prices{
-    margin-top:200px;
-  }
-
-  .works img{
-    width:80%;
-  }
-  .cont1{
-    width:90%;
-  }
-
-  .prices .card{
-    width:90%;
-  }
-  .card img{
-    width:200px;
-  }
-  .cont1 p{
-    font-size:20px;
-  }
-
-  .prices .card{
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    text-align:center;
-  }
-  
-  .prices .card h2,.prices .card .info,.prices .card .price{
-    width:300px;
-  }
-  .home{
-    min-height:120vh;
-  }
+.testimonial-card h3 {
+  margin-top: 20px;
+  font-size: 18px;
+  color: #000;
 }
 </style>
