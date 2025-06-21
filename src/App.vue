56c6684c09b270367b7f864b05b63b0d8b22a1bb
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-21 18:25:59 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup>
-import { RouterView, useRoute } from 'vue-router'
-import NavBar from '@/components/NavBar.vue'
-
-const route = useRoute();
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- إظهار NavBar في كل الصفحات ما عدا ProductsPlace -->
-  <NavBar v-if="route.name !== 'ProductsPlace'" />
   <RouterView />
 </template>
 
 <style scoped>
-/* يمكنك إضافة تنسيقات عامة هنا إذا أردت */
 </style>
