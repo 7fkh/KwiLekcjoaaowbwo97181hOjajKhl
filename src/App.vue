3b1b08c1b7a4d3d4a042c8b025f098a0455c9e83
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-22 15:57:14 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 ‏<script setup>
-‏import { RouterView } from 'vue-router'
-‏import { ref } from 'vue'
-
-// بدلاً من استيراد KhliLoding، سنجربه بدون loading أولاً
-‏const isPageLoading = ref(false) // مؤقتاً لا نظهر loading
+‏import { RouterLink, RouterView } from 'vue-router'
 ‏</script>
 
 ‏<template>
-  <!-- معلق مؤقتاً -->
-‏  <!-- <KhliLoding 
-‏    :isLoading="isPageLoading"
-‏    :loadingText="'جاري تحميل المتجر...'"
-‏    :duration="5000"
-  /> -->
-  
 ‏  <RouterView />
 ‏</template>
 
