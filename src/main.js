076e4e0ff650f10c5a188db3adc6fa214b26cad8
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-22 15:25:39 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import KhliLoding from '@/components/khliLoding.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
