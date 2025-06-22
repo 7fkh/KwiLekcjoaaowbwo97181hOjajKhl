38d7dae86fe40fd59d66967bc65cf9a723ba1277
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-22 15:41:26 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
+‏<script setup>
+‏import { RouterLink, RouterView } from 'vue-router'
+‏import { ref, onMounted } from 'vue'
+‏import KhliLoding from '@/components/khliLoding.vue'
 
-<template>
-  <RouterView />
-</template>
+// متغيرات التحميل
+‏const isPageLoading = ref(true)
 
-<style scoped>
-</style>
+// دالة انتهاء التحميل
+‏const onLoadingComplete = () => {
+‏  isPageLoading.value = false
+‏  console.log('انتهى التحميل!')
+}
+
+
+‏onMounted(() => {
+
+})
+‏</script>
+
+‏<template>
+‏  <KhliLoding 
+‏    :isLoading="isPageLoading"
+‏    :loadingText="'جاري تحميل المتجر...'"
+‏    :duration="5000"
+‏    @loading-complete="onLoadingComplete"
+  />
+  
+ 
+‏  <RouterView />
+‏</template>
+
+‏<style scoped>
+‏</style>
