0c3557668298260fdc09e091682aea624cb4dd03
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-30 12:11:15 +0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -144,7 +144,7 @@ export default {
   background-size: 400% 400%;
   animation: gradientMove 8s ease infinite;
   position: relative;
-  min-height: 90px;
+  min-height: 120px;
   overflow: visible;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
 }
