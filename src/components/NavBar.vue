70a8755dc858c11fd2913acc72c36dc1babc4223
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-30 12:02:49 +0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -141,7 +141,7 @@ export default {
 /* الخلفية الرئيسية */
 .control-background.home {
   background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
-  background-size: 100% 100%;
+  background-size: 400% 400%;
   animation: gradientMove 8s ease infinite;
   position: relative;
   min-height: 120px;
