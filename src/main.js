f96ae5240f9c1b267719c044c6c1542b3f791dd2
7fkh <155654712+7fkh@users.noreply.github.com>
2025-08-04 12:51:13 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { Analytics } from "@vercel/analytics/next"
 
 library.add(fas)
 
