01ae186f90b4c07006323b623af69d8a4fb6d4e1
7fkh <155654712+7fkh@users.noreply.github.com>
2025-08-05 07:22:39 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsPlace from '@/components/ProductsPlace.vue'
+import OrderView from '@/views/OrderView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
