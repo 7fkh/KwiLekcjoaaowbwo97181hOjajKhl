1df5f8d674b3fadaab0b1932454931d68a7d5752
7fkh <155654712+7fkh@users.noreply.github.com>
2025-08-05 04:13:58 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,33 +7,12 @@ import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { inject } from "@vercel/analytics"
 
 library.add(fas)
 
-// إنشاء التطبيق
 const app = createApp(App)
 
-// استخدام Router
 app.use(router)
-
-// تكوين الخصائص العامة
-app.config.globalProperties.$formatPrice = function(price) {
-  return new Intl.NumberFormat('ar-SA', {
-    style: 'currency',
-    currency: 'SAR',
-    minimumFractionDigits: 0
-  }).format(price)
-}
-
-// معالج الأخطاء العام
-app.config.errorHandler = (error, instance, info) => {
-  console.error('خطأ في التطبيق:', error)
-  console.error('معلومات الخطأ:', info)
-}
-
-// تحسينات الأداء
-app.config.performance = true
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
