424f35cc9c414b70c5c6aaa017f54b7015b775ad
7fkh <155654712+7fkh@users.noreply.github.com>
2025-08-05 07:24:04 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProductsPlace from '@/components/ProductsPlace.vue'
-import OrderView from '@/views/OrderView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/order',
+    name: 'order',
+    component: () => import('../views/OrderView.vue'),
+    props: true
+  },
+  {
+    path: '/policy',
+    name: 'policy',
+    component: () => import('../components/policy.vue')
+  },
+  {
+    path: '/laws',
+    name: 'laws',
+    component: () => import('../views/laws.vue')
+  },
+  {
+    path: '/products',
+    name: 'product',
+    component: () => import('../views/Products.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/order',
-      name: 'order',
-      component: () => import('../views/OrderView.vue')
-      props: true
-    },
-{
-      path: '/policy',
-      name: 'policy',
-      component: () => import('../components/policy.vue')
-    },
-{
-      path: '/laws',
-      name: 'laws',
-      component: () => import('../views/laws.vue')
-    },
-    {
-  path: '/products',
-  name: 'product',
-  component: () => import('../views/Products.vue')
-}
-  ]
-})
-
-const router = createRouter({
-  history: createWebHistory(),
   routes
 })
 
 export default router
+
