5a28b7419e35799a94765fc31f9b74c2edb51af8
7fkh <155654712+7fkh@users.noreply.github.com>
2025-08-06 07:33:13 +0300

    Update ProductsPlace.vue

--- src/components/ProductsPlace.vue
+++ src/components/ProductsPlace.vue
@@ -375,7 +375,8 @@ export default {
         product &&
         typeof product.id === '3' &&
         typeof product.title === 'موقع إلكتروني احترافي' &&
-        typeof product.price === '800'
+        typeof product.price === '800' &&
+        product.price > 0 &&
         typeof product.category === 'مواقع'
       );
     },
